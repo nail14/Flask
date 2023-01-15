4c190dd2328ce556fff0a94fd6ecfef5850c94fb
nail14 <nail_14@mail.ru>
2023-01-15 21:50:05 +0300

    lesson-5

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,3 @@
-
 from flask_login import UserMixin
 
 from blog.app import db
@@ -9,9 +8,12 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True)
-    name = db.Column(db.String(255))
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
     password = db.Column(db.String(255))
 
-    def __init__(self, email, password):
+    def __init__(self, email, first_name, last_name, password):
         self.email = email
         self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
--- wsgi.py
+++ wsgi.py
@@ -1,23 +1,3 @@
-from werkzeug.security import generate_password_hash
-
-from blog.app import create_app, db
+from blog.app import create_app
 
 app = create_app()
-# app.config.from_pyfile('config.py')
-
-
-# function will create all tables
-@app.cli.command('init-db')
-def init_db():
-    db.create_all()
-
-
-# add users
-@app.cli.command('create-users')
-def create_user():
-    from blog.models import User
-
-    db.session.add(
-        User(email='nail5552@email.ru', password=generate_password_hash('test123'))
-    )
-    db.session.commit()
