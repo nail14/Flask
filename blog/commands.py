5f78d7ddf367c5b024aa1979c6f2d7300b0cec33
nail14 <nail_14@mail.ru>
2023-01-19 20:15:56 +0300

    lesson-6

--- blog/commands.py
+++ blog/commands.py
@@ -11,6 +11,6 @@ def create_init_user():
 
     with app.app_context():
         db.session.add(
-            User(email='name@example.com', password=generate_password_hash('test123'))
+            User(email='nail@mail.ru', password=generate_password_hash('test123'))
         )
         db.session.commit()
