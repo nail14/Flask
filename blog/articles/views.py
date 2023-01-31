66d3b38e253dd67a26da004f75dc61b7f441d53e
nail14 <nail_14@mail.ru>
2023-01-31 21:16:05 +0300

    some fix

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,3 +1,6 @@
+from typing import Dict
+
+import requests
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
 from sqlalchemy.orm import joinedload
@@ -13,9 +16,13 @@ article = Blueprint('article', __name__, url_prefix='/articles', static_folder='
 @article.route('/', methods=['GET'])
 def article_list():
     articles: Article = Article.query.all()
+    # call RPC method
+    count_articles: Dict = requests.get('http://127.0.0.1:5000/api/articles/event_get_count/').json()
     return render_template(
         'articles/list.html',
         articles=articles,
+        count_articles=count_articles['count'],
+
     )
 
 
